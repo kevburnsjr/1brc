82354a8df23555db09296612bce3a563714f890f
kevburnsjr <kevburnsjr@gmail.com>
2024-01-31 19:20:25 -0800

    First pass

---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"slices"
+	"strconv"
+)
+
+type entry struct {
+	count int
+	total int
+	min   int
+	max   int
+	name  []byte
+}
+
+type result struct {
+	entries map[string]entry
+	buffer  []byte
+}
+
+var results chan result
+
+func main() {
+	f, err := os.Open(`../1brc/data/full.txt`)
+	// f, err := os.Open(`../1brc/data/test.txt`)
+	if err != nil {
+		panic(err)
+	}
+	c := runtime.NumCPU() / 4
+	run(c, 64*1024*1024, f)
+}
+
+func run(c, size int, f io.Reader) {
+	var r result
+	var ok bool
+	var fe entry
+	results = make(chan result, c)
+	for i := 0; i < c; i++ {
+		results <- result{
+			map[string]entry{},
+			make([]byte, size),
+		}
+	}
+	read(f)
+	final := map[string]entry{}
+	for i := 0; i < c; i++ {
+		select {
+		case r = <-results:
+			for name, e := range r.entries {
+				fe, ok = final[name]
+				if !ok {
+					fe.name = []byte(name)
+					fe.max = -1e6
+					fe.min = 1e6
+				}
+				fe.total += e.total
+				fe.count += e.count
+				if e.max > fe.max {
+					fe.max = e.max
+				}
+				if e.min < fe.min {
+					fe.min = e.min
+				}
+				final[name] = fe
+			}
+		}
+	}
+	var entries []entry
+	for _, e := range final {
+		entries = append(entries, e)
+	}
+	slices.SortFunc(entries, func(a, b entry) int {
+		return bytes.Compare(a.name, b.name)
+	})
+	fmt.Print(`{`)
+	for i, e := range entries {
+		if bytes.HasPrefix(e.name, []byte(`\n`)) {
+			continue
+		}
+		fmt.Printf(`%s=%.1f/%.1f/%.1f`, e.name, float64(e.min)/10, float64(e.total)/float64(e.count)/10, float64(e.max)/10)
+		if i < len(entries)-1 {
+			fmt.Print(`, `)
+		}
+	}
+	fmt.Print(`}`)
+}
+
+func read(f io.Reader) {
+	var r result
+	var n int
+	var off int
+	var rem = make([]byte, 100)
+	var err error
+	for {
+		select {
+		case r = <-results:
+			copy(r.buffer[:off], rem[:off])
+			n, err = f.Read(r.buffer[off:cap(r.buffer)])
+			n += off
+			off = n - bytes.LastIndexByte(r.buffer[:n], '\n')
+			copy(rem[:off], r.buffer[n-off:n])
+			r.buffer = r.buffer[:n-off]
+			go doPart(r)
+			if err == io.EOF {
+				return
+			}
+		}
+	}
+}
+
+func doPart(r result) {
+	var i int
+	var j int
+	var k int
+	var f float64
+	var temp int
+	var name string
+	var e entry
+	var ok bool
+	for i < len(r.buffer) {
+		j = bytes.IndexByte(r.buffer[i:], ';') + i
+		name = string(r.buffer[i:j])
+		e, ok = r.entries[name]
+		if !ok {
+			e.max = -1e6
+			e.min = 1e6
+		}
+		k = bytes.IndexByte(r.buffer[i:], '\n') + i
+		if j > k {
+			k = len(r.buffer)
+		}
+		f, _ = strconv.ParseFloat(string(r.buffer[j+1:k]), 32)
+		temp = int(f * 10)
+		e.count++
+		e.total += temp
+		if temp < e.min {
+			e.min = temp
+		}
+		if temp > e.max {
+			e.max = temp
+		}
+		r.entries[name] = e
+		i = k + 1
+	}
+	results <- r
+	return
+}
